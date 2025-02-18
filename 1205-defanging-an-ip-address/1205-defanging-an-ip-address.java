1e9efcfe6cdd4780677ed6638f6d63da83692eae
BHAVISHYA VERMA <148390371+bhavishyaverma450@users.noreply.github.com>
2025-02-18 14:27:00 +0530

    Time: 0 ms (100%), Space: 41.5 MB (60.54%) - LeetHub

--- 1205-defanging-an-ip-address/1205-defanging-an-ip-address.java
+++ 1205-defanging-an-ip-address/1205-defanging-an-ip-address.java
@@ -1,6 +1,24 @@
+//using string
+// class Solution {
+//     public String defangIPaddr(String address) {
+//         String str = address.replace(".","[.]");
+//         return str;
+//     }
+// }
+
+//using string builder
 class Solution {
     public String defangIPaddr(String address) {
-        String str = address.replace(".","[.]");
-        return str;
+        StringBuilder sb = new StringBuilder();
+        for(char i:address.toCharArray())
+        {
+            if(i == '.')
+            {
+                sb.append("[.]");
+            }else{
+                sb.append(i);
+            }
+        }
+    return sb.toString();
     }
 }
