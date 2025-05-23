7835e817647442020d2b8295f1c187dc054f90e2
BHAVISHYA VERMA <148390371+bhavishyaverma450@users.noreply.github.com>
2025-05-23 18:12:28 +0530

    Time: 0 ms (100%), Space: 46.2 MB (16.7%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,35 +1,74 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int res[] = new int[2];
+        
+        int[] res = new int[2];
         res[0]=-1;
         res[1]=-1;
-        int n = nums.length;
-        int strt=0;
-        int end=n-1;
-        while(strt<=end){
-            int mid = strt+(end-strt)/2;
+        
+        int left=0;
+        int right=nums.length-1;
+        while(left<=right){
+            int mid=left+(right-left)/2;
             if(nums[mid]==target){
                 res[0]=mid;
-                end = mid-1;
-            }else if(nums[mid]<target){
-                strt = mid+1;
+                right=mid-1;
+            }else if(nums[mid]>target){
+                right=mid-1;
             }else{
-                end = mid-1;
+                left=mid+1;
             }
         }
-        strt=0;
-        end=n-1;
-        while(strt<=end){
-            int mid = strt+(end-strt)/2;
+        left=0;
+        right=nums.length-1;
+        while(left<=right){
+            int mid=left+(right-left)/2;
             if(nums[mid]==target){
                 res[1]=mid;
-                strt = mid+1;
-            }else if(nums[mid]<target){
-                strt = mid+1;
+                left=mid+1;
+            }else if(nums[mid]>target){
+                right=mid-1;
             }else{
-                end = mid-1;
+                left=mid+1;
             }
         }
+        
+        
+        
+        
+        
         return res;
+        
+        
+        // int res[] = new int[2];
+        // res[0]=-1;
+        // res[1]=-1;
+        // int n = nums.length;
+        // int strt=0;
+        // int end=n-1;
+        // while(strt<=end){
+        //     int mid = strt+(end-strt)/2;
+        //     if(nums[mid]==target){
+        //         res[0]=mid;
+        //         end = mid-1;
+        //     }else if(nums[mid]<target){
+        //         strt = mid+1;
+        //     }else{
+        //         end = mid-1;
+        //     }
+        // }
+        // strt=0;
+        // end=n-1;
+        // while(strt<=end){
+        //     int mid = strt+(end-strt)/2;
+        //     if(nums[mid]==target){
+        //         res[1]=mid;
+        //         strt = mid+1;
+        //     }else if(nums[mid]<target){
+        //         strt = mid+1;
+        //     }else{
+        //         end = mid-1;
+        //     }
+        // }
+        // return res;
     }
 }
