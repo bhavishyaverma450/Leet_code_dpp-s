cf786c1973d9f4844179cc68bf8bad425cfc64c5
BHAVISHYA VERMA <148390371+bhavishyaverma450@users.noreply.github.com>
2025-08-23 14:48:19 +0530

    Time: 16 ms (61.83%), Space: 44.9 MB (47.31%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,37 +1,28 @@
-class Solution {
-    public String longestPalindrome(String s) {
-        int max_palindrom_left=0;
-        int max_palindrom_right=0;
-        for(int i=1;i<s.length()-1;i++){
-            int palindrom_left=0;
-            int palindrom_right=0;
-            int left = i-1;
-            int right=i+1;
-            while(left>=0 && right<s.length()){
-                if(s.charAt(left) == s.charAt(right)){
-                    palindrom_left = left;
-                    palindrom_right=right;
-                    if(--left !=-1){
-                        left--;
-                    }
-                    if(++right !=s.length()){
-                        right++;
-                    }
-                }else if(s.charAt(left)==s.charAt(i)){
-                    if(--left !=-1){
-                        left--;
-                    }
-                }else if(s.charAt(i)==s.charAt(right)){
-                    if(++right !=s.length()){
-                        right++;
-                    }
-                }else{
-                    break;
-                }
-                max_palindrom_left = Math.min(palindrom_left,max_palindrom_left);
-                max_palindrom_right = Math.max(palindrom_right,max_palindrom_right);
+class Solution{
+    public String longestPalindrome(String s){
+        if(s==null || s.length()<2) return s;
+
+        int maxLeft=0;
+        int maxRight=0;
+        for(int i=0;i<s.length();i++){
+            int[] odd = expandFromCenter(s,i,i);
+            if(odd[1]-odd[0]>maxRight-maxLeft){
+                maxLeft=odd[0];
+                maxRight=odd[1];
+            }
+            int[] even = expandFromCenter(s,i,i+1);
+            if(even[1]-even[0]>maxRight-maxLeft){
+                maxLeft=even[0];
+                maxRight = even[1];
             }
         }
-        return s.substring(max_palindrom_left,max_palindrom_right);
+        return s.substring(maxLeft,maxRight+1);
+    }
+    public int[] expandFromCenter(String s,int left,int right){
+        while(left>=0 && right<s.length() && (s.charAt(left)==s.charAt(right))){
+            left--;
+            right++;
+        }
+        return new int[]{left+1,right-1};
     }
 }
