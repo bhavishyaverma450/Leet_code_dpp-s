822e660ac583323d4360504d58571f9ba83292a6
BHAVISHYA VERMA <148390371+bhavishyaverma450@users.noreply.github.com>
2025-08-31 19:34:58 +0530

    Time: 3 ms (91.62%), Space: 42.8 MB (94.58%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,17 +1,17 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
         if(s.length()!=t.length()) return false;
-        
-        int[] alphabet = new int[26];
-
+        int[] alphabats = new int[26];
         for(int i=0;i<s.length();i++){
-            alphabet[s.charAt(i)-'a']++;
+            alphabats[s.charAt(i)-'a']++;
         }
         for(int i=0;i<t.length();i++){
-            alphabet[t.charAt(i)-'a']--;
+            alphabats[t.charAt(i)-'a']--;
         }
-        for(int i:alphabet){
-            if(i!=0) return false;
+        for(int i=0;i<alphabats.length;i++){
+            if(alphabats[i]!=0){
+                return false;
+            }
         }
         return true;
     }
