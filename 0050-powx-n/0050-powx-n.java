1a702d13a3e4d776be08777aff35bf6288b516a8
BHAVISHYA VERMA <148390371+bhavishyaverma450@users.noreply.github.com>
2025-09-09 11:04:39 +0530

    Time: 0 ms (100%), Space: 44 MB (5.07%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,5 +1,20 @@
 class Solution {
     public double myPow(double x, int n) {
-        return Math.pow(x,n);
+        // return Math.pow(x,n);
+        long N = n;
+        return exp(x,N);    
+    }
+    public double exp(double x,long n){
+        if(n==0){
+            return 1;
+        }
+        if (n < 0) {
+            return 1.0 / exp(x, -n);
+        }
+        if(n%2==1){
+            return x*exp(x*x,(n-1)/2);
+        }else{
+            return exp(x*x,n/2);
+        }
     }
 }
