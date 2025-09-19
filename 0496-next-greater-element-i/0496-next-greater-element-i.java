ef2ae43ebf7b720421836cc8f962f83b44345684
BHAVISHYA VERMA <148390371+bhavishyaverma450@users.noreply.github.com>
2025-09-19 18:25:05 +0530

    Time: 7 ms (6.09%), Space: 43.2 MB (91.45%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,27 +1,42 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        int nextEle=0;
-        int ele=0;
-        for(int i=0;i<nums1.length;i++){
-            int index=-1;
-            for(int j=0;j<nums2.length;j++){
-                if(nums2[j]==nums1[i]){
-                    index=j;
-                    break;
-                }
+        Stack<Integer> st = new Stack<>();
+        int[] res = new int[nums2.length];
+        int k=nums2.length-1;
+        for(int i=nums2.length-1;i>=0;i--){
+            if(st.isEmpty()){
+                res[k]=-1;
+                k--;
+                st.push(nums2[i]);
             }
-            boolean found=false;
-            for(int k=index+1;k<nums2.length;k++){
-                if(nums2[k]>nums1[i]){
-                    nums1[i]=nums2[k];
-                    found=true;
-                    break;
-                }
+            else if(st.peek()>nums2[i]){
+                res[k]=st.peek();
+                k--;
+                st.push(nums2[i]);
             }
-            if(!found){
-                nums1[i]=-1;
+            else if(st.peek()<=nums2[i]){
+                while(!st.isEmpty() && st.peek()<=nums2[i]){
+                    st.pop();
+                }
+                if(st.isEmpty()){
+                    res[k]=-1;
+                }else{
+                    res[k]=st.peek();
+                }
+                k--;
+                st.push(nums2[i]);
             }
         }
-        return nums1;
+        k=0;
+        int[] ans = new int[nums1.length];
+        for(int i=0;i<ans.length;i++){
+            for(int j=0;j<res.length;j++){
+                if(nums2[j]==nums1[i]){
+                    ans[k]=res[j];
+                    k++;
+                }
+            }
+        }
+        return ans;
     }
 }
