ab2c476a7e3aa4cb421d75ddc6e78bbabfb49532
BHAVISHYA VERMA <148390371+bhavishyaverma450@users.noreply.github.com>
2025-11-26 22:42:34 +0530

    Time: 2 ms (99.37%), Space: 42.2 MB (78.5%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.java
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.java
@@ -3,8 +3,8 @@ class Solution {
         StringBuilder sb = new StringBuilder();
         int opened=0;
         for(char ch: s.toCharArray()){
-            if(ch == '(' && opened++>0) sb.append(ch);
-            if(ch == ')' && opened-->1) sb.append(ch);
+            if(ch=='(' && opened++>0) sb.append(ch);
+            if(ch==')' && opened-->1) sb.append(ch);
         }
         return sb.toString();
     }
