8d9e542f68c1e33d01a3c8eb34d8f398aef7e323
BHAVISHYA VERMA <148390371+bhavishyaverma450@users.noreply.github.com>
2026-01-24 14:42:12 +0530

    Time: 0 ms (100%), Space: 43.8 MB (41.72%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,24 +1,23 @@
 class Solution {
     public void rotate(int[][] matrix) {
-        int n = matrix.length;
-
-        for(int i=0;i<n;i++){
-            for(int j=i;j<n;j++){
-                int temp = matrix[i][j];
-                matrix[i][j] = matrix[j][i];
-                matrix[j][i] = temp;
+        for(int i=0;i<matrix.length;i++){
+            for(int j=i+1;j<matrix[0].length;j++){
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
             }
         }
-        for(int i=0;i<n;i++){
-            int start = 0;
-            int end = n-1;
-            while(start<=end){
-                int temp = matrix[i][start];
-                matrix[i][start] = matrix[i][end];
-                matrix[i][end] = temp;
-                start++;
-                end--;
-            }
+        for(int i=0;i<matrix.length;i++){
+            reverse(matrix[i]);
+        }
+    }
+    public void reverse(int[] arr){
+        int s=0,e=arr.length-1;
+        while(s<=e){
+            int temp=arr[s];
+            arr[s]=arr[e];
+            arr[e]=temp;
+            s++;e--;
         }
     }
 }
