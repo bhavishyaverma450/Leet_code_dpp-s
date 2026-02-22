5dd7e7db58deb1d26eff064eec98cfe41dedc1d3
BHAVISHYA VERMA <148390371+bhavishyaverma450@users.noreply.github.com>
2026-02-22 11:58:26 +0530

    Time: 1 ms (100%), Space: 46.7 MB (34.75%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.java
+++ 0695-max-area-of-island/0695-max-area-of-island.java
@@ -3,7 +3,7 @@ class Solution {
         int m=grid.length;
         int n=grid[0].length;
         int maxArea=0;
-
+        
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(grid[i][j]==1){
@@ -17,15 +17,15 @@ class Solution {
     public int dfs(int[][] grid,int i,int j){
         int m=grid.length;
         int n=grid[0].length;
-        if(i<0 || j<0 || i>=m || j>=n || grid[i][j]==0)return 0;
-        int t=grid[i][j];
+
+        if(i<0 || i>=m || j<0 || j>=n || grid[i][j]==0)return 0;
         grid[i][j]=0;
 
-        int top=dfs(grid,i-1,j);
         int left=dfs(grid,i,j-1);
         int right=dfs(grid,i,j+1);
         int bottom=dfs(grid,i+1,j);
+        int up=dfs(grid,i-1,j);
 
-        return 1+top+left+right+bottom;
+        return 1+left+right+bottom+up;
     }
 }
