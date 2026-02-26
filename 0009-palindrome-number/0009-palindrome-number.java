8dd40427ba1f180e263e5019721dc2347ed369f1
BHAVISHYA VERMA <148390371+bhavishyaverma450@users.noreply.github.com>
2026-02-26 23:35:01 +0530

    Time: 6 ms (23.15%), Space: 46.1 MB (32.99%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,11 +1,11 @@
 class Solution {
     public boolean isPalindrome(int x) {
         String str=Integer.toString(x);
-        return isPalindrome(str,0,str.length()-1);
+        return palin(str,0,str.length()-1);
     }
-    public boolean isPalindrome(String str,int s,int e){
+    public boolean palin(String str,int s,int e){
         if(s>e)return true;
         if(str.charAt(s)!=str.charAt(e))return false;
-        else return isPalindrome(str,s+1,e-1);
+        return palin(str,s+1,e-1);
     }
 }
