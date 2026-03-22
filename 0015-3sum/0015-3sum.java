27cc6020428bc17385e7fcf512bc67d3b3b4f660
BHAVISHYA VERMA <148390371+bhavishyaverma450@users.noreply.github.com>
2026-03-22 15:13:31 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -3,7 +3,7 @@ class Solution {
         List<List<Integer>> list=new ArrayList<>();
         Arrays.sort(nums);
 
-        for(int i=0;i<nums.length-2;i++){
+        for(int i=0;i<nums.length;i++){
             if(i>0 && nums[i]==nums[i-1])continue;
             int left=i+1;
             int right=nums.length-1;
@@ -13,12 +13,11 @@ class Solution {
                     list.add(Arrays.asList(nums[i],nums[left],nums[right]));
 
                     while(left<right && nums[left]==nums[left+1])left++;
-                    while(left<right && nums[right]==nums[right-1])right--;
+                    while(left<right && nums[right]==nums[right-1])right--; 
 
                     left++;right--;
-                }else if(sum>0){
-                    right--;
-                }else left++;
+                }else if(sum>0)right--;
+                else left++;
             }
         }
         return list;
