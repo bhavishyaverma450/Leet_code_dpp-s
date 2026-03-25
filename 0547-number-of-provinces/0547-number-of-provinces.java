7af62e541a207dddb90885c0f4b6b4f90d4f7d33
BHAVISHYA VERMA <148390371+bhavishyaverma450@users.noreply.github.com>
2026-03-26 00:53:33 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,11 +1,11 @@
 class Solution {
     public int findCircleNum(int[][] isConnected) {
-        int n=isConnected.length;
-        boolean[] vis=new boolean[n];
-        Arrays.fill(vis,false);
         int count=0;
+        ArrayList<ArrayList<Integer>> adj=convert(isConnected);
 
-        List<List<Integer>> adj=convert(isConnected);
+        boolean[] vis=new boolean[adj.size()];
+        Arrays.fill(vis,false);
+
         for(int i=0;i<adj.size();i++){
             if(!vis[i]){
                 dfs(i,adj,vis);
@@ -14,19 +14,23 @@ class Solution {
         }
         return count;
     }
-    public List<List<Integer>> convert(int[][] arr){
-        List<List<Integer>> adj=new ArrayList<>();
-        for(int i=0;i<arr.length;i++){
+    public ArrayList<ArrayList<Integer>> convert(int[][] arr){
+        int n=arr.length;
+        ArrayList<ArrayList<Integer>> adj=new ArrayList<>();
+
+        for(int i=0;i<n;i++){
             adj.add(new ArrayList<>());
         }
-        for(int i=0;i<arr.length;i++){
-            for(int j=0;j<arr.length;j++){
-                if(arr[i][j]==1 && i!=j)adj.get(i).add(j);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(arr[i][j]==1 && i!=j){
+                    adj.get(i).add(j);
+                }
             }
         }
         return adj;
     }
-    public void dfs(int node,List<List<Integer>> adj,boolean[] vis){
+    public void dfs(int node,ArrayList<ArrayList<Integer>> adj,boolean[] vis){
         vis[node]=true;
         for(int it:adj.get(node)){
             if(!vis[it]){
