e72f4c1698facb260aa04497e14c8dd8010e17cf
BHAVISHYA VERMA <148390371+bhavishyaverma450@users.noreply.github.com>
2026-04-03 00:09:13 +0530

    Time: 13 ms (27.28%), Space: 46.2 MB (46.68%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,5 +1,6 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length())return false;
         HashMap<Character,Integer> map1=new HashMap<>();
         HashMap<Character,Integer> map2=new HashMap<>();
 
@@ -9,7 +10,6 @@ class Solution {
         for(char ch:t.toCharArray()){
             map2.put(ch,map2.getOrDefault(ch,0)+1);
         }
-
         if(map1.equals(map2))return true;
         return false;
     }
