e0df8dec931d35b055594e79fb7a8e1ac8636f00
BHAVISHYA VERMA <148390371+bhavishyaverma450@users.noreply.github.com>
2026-04-06 21:46:37 +0530

    Time: 1337 ms (10.26%), Space: 50.9 MB (59.48%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
@@ -4,13 +4,9 @@ class Solution {
         for(int i=0;i<nums.length;i++){
             int sum=0;
             for(int j=i;j<nums.length;j++){
-                sum += nums[j];
-                if(sum==goal){
-                    count++;
-                }
-                if(sum>goal){
-                    break;
-                }
+                sum+=nums[j];
+                if(sum==goal)count++;
+                else if(sum>goal)break;
             }
         }
         return count;
