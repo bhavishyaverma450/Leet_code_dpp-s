68b110ebfbbb35a3a6a4a252473382016faf24ad
BHAVISHYA VERMA <148390371+bhavishyaverma450@users.noreply.github.com>
2026-04-07 22:12:46 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -4,26 +4,21 @@ class Solution {
         int n=grid[0].length;
         int time=0;
         int freshCount=0;
-
         Queue<int[]> q=new LinkedList<>();
-
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(grid[i][j]==1)freshCount++;
-                else if(grid[i][j]==2){
-                    q.add(new int[]{i,j});     
-                }
+                else if(grid[i][j]==2)q.add(new int[]{i,j});
             }
         }
-
         int[] dx={-1,0,1,0};
         int[] dy={0,-1,0,1};
         while(!q.isEmpty() && freshCount>0){
             int size=q.size();
             for(int i=0;i<size;i++){
-                int[] temp=q.poll();
-                int x=temp[0];
-                int y=temp[1];
+                int[] curr=q.poll();
+                int x=curr[0];
+                int y=curr[1];
                 for(int j=0;j<4;j++){
                     int newX=x+dx[j];
                     int newY=y+dy[j];
@@ -36,6 +31,6 @@ class Solution {
             }
             time++;
         }
-        return (freshCount==0)?time:-1;
+        return freshCount==0?time:-1;
     }
 }
