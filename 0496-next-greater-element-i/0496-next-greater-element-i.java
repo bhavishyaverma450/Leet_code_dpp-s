ac3e137aacc61faea649dab74238f33d85f73433
BHAVISHYA VERMA <148390371+bhavishyaverma450@users.noreply.github.com>
2026-04-29 13:15:32 +0530

    Time: 23 ms (5.29%), Space: 46.3 MB (10.84%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,42 +1,36 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        Stack<Integer> st = new Stack<>();
-        int[] res = new int[nums2.length];
-        int k=nums2.length-1;
+        int[] arr=new int[nums2.length];
+        Stack<Integer> st=new Stack<>();
         for(int i=nums2.length-1;i>=0;i--){
             if(st.isEmpty()){
-                res[k]=-1;
-                k--;
+                arr[i]=-1;
                 st.push(nums2[i]);
-            }
-            else if(st.peek()>nums2[i]){
-                res[k]=st.peek();
-                k--;
-                st.push(nums2[i]);
-            }
-            else if(st.peek()<=nums2[i]){
-                while(!st.isEmpty() && st.peek()<=nums2[i]){
+            }else if(st.peek()<nums2[i]){
+                while(!st.isEmpty() && st.peek()<nums2[i]){
                     st.pop();
                 }
-                if(st.isEmpty()){
-                    res[k]=-1;
-                }else{
-                    res[k]=st.peek();
-                }
-                k--;
+                if(st.isEmpty())arr[i]=-1;
+                else arr[i]=st.peek();
+                st.push(nums2[i]);
+            }else if(st.peek()>nums2[i]){
+                arr[i]=st.peek();
                 st.push(nums2[i]);
             }
         }
-        k=0;
-        int[] ans = new int[nums1.length];
-        for(int i=0;i<ans.length;i++){
-            for(int j=0;j<res.length;j++){
-                if(nums2[j]==nums1[i]){
-                    ans[k]=res[j];
-                    k++;
+        for(int it:arr){
+            System.out.print(it+" ");
+        }
+        int[] res=new int[nums1.length];
+        for(int i=0;i<nums1.length;i++){
+            int ele=nums1[i];
+            for(int j=0;j<nums2.length;j++){
+                if(ele==nums2[j]){
+                    res[i]=arr[j];
+                    break;
                 }
             }
         }
-        return ans;
+        return res;
     }
 }
