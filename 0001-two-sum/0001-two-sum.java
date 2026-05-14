f8251f23808b536c024d247c9c5dd8711ea8dee0
BHAVISHYA VERMA <148390371+bhavishyaverma450@users.noreply.github.com>
2026-05-15 03:12:28 +0530

    Time: 2 ms (99.38%), Space: 47 MB (58.12%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -4,7 +4,8 @@ class Solution {
         for(int i=0;i<nums.length;i++){
             if(map.containsKey(target-nums[i])){
                 return new int[]{map.get(target-nums[i]),i};
-            }else map.put(nums[i],i);
+            }
+            else map.put(nums[i],i);
         }
         return new int[]{-1,-1};
     }
