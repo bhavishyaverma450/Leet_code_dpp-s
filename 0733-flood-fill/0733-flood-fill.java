baab559c32c56b068b24bbee0c6f3c4ac80f5128
BHAVISHYA VERMA <148390371+bhavishyaverma450@users.noreply.github.com>
2026-07-09 02:06:11 +0530

    Time: 1 ms (39%), Space: 46.8 MB (43.78%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,19 +1,30 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        if(image[sr][sc]==color)return image;
-        dfs(image,sr,sc,color,image[sr][sc]);
-        return image;
-    }
-    public void dfs(int[][] image,int i,int j,int color,int val){
         int m=image.length;
         int n=image[0].length;
+        
+        int t=image[sr][sc];
+        if(t==color)return image;
+        Queue<int[]> q=new LinkedList<>();
+        q.add(new int[]{sr,sc});
+        image[sr][sc]=color;
 
-        if(i<0 || i>=m || j<0 || j>=n || image[i][j]!=val)return;
-        image[i][j]=color;
+        int[] dx={-1,1,0,0};
+        int[] dy={0,0,1,-1};
 
-        dfs(image,i+1,j,color,val);
-        dfs(image,i,j+1,color,val);
-        dfs(image,i-1,j,color,val);
-        dfs(image,i,j-1,color,val);
+        while(!q.isEmpty()){
+            int[] curr=q.poll();
+            int x=curr[0];
+            int y=curr[1];
+            for(int i=0;i<4;i++){
+                int newX=x+dx[i];
+                int newY=y+dy[i];
+                if(newX>=0 && newX<m && newY>=0 && newY<n && image[newX][newY]==t){
+                    q.add(new int[]{newX,newY});
+                    image[newX][newY]=color;
+                }
+            }
+        }
+        return image;
     }
 }
