cad1edd102e54145a92c467ecbc00e57aad93bd6
BHAVISHYA VERMA <148390371+bhavishyaverma450@users.noreply.github.com>
2026-07-13 16:28:38 +0530

    Time: 2 ms (43.12%), Space: 47.5 MB (31.54%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,41 +1,43 @@
 class Solution {
+    class DSU{
+        int[] parent;
+        int[] size;
+        public DSU(int n){
+            parent=new int[n];
+            size=new int[n];
+            for(int i=0;i<n;i++){
+                parent[i]=i;
+                size[i]=1;
+            }
+        }
+        public int find(int node){
+            if(parent[node]==node)return node;
+            return parent[node]=find(parent[node]);
+        }
+        public void union(int u,int v){
+            int upu=find(u);
+            int upv=find(v);
+            if(size[upu]>=size[upv]){
+                parent[upv]=parent[upu];
+                size[upu]+=size[upv];
+            }else{
+                parent[upu]=parent[upv];
+                size[upv]+=size[upu];
+            }
+        }
+    }
     public int findCircleNum(int[][] isConnected) {
         int n=isConnected.length;
-        List<List<Integer>> adj=convert(isConnected);
         int count=0;
-        boolean[] vis=new boolean[n];
-        Arrays.fill(vis,false);
-
-        for(int i=0;i<adj.size();i++){
-            if(!vis[i]){
-                dfs(i,adj,vis);
-                count++;
-            }
-        }
-        return count;
-    }
-    public void dfs(int node,List<List<Integer>> adj,boolean[] vis){
-        vis[node]=true;
-        for(int t:adj.get(node)){
-            if(!vis[t]){
-                vis[t]=true;
-                dfs(t,adj,vis);
-            }
-        }
-    }
-    public List<List<Integer>> convert(int[][] arr){
-        int n=arr.length;
-        List<List<Integer>> adj=new ArrayList<>();
-        for(int i=0;i<n;i++){
-            adj.add(new ArrayList<>());
-        }
+        DSU dsu = new DSU(n);
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
-                if(arr[i][j]==1 && i!=j){
-                    adj.get(i).add(j);
-                }
+                if(isConnected[i][j]==1)dsu.union(i,j);
             }
         }
-        return adj;
+        for(int i=0;i<dsu.parent.length;i++){
+            if(dsu.parent[i]==i)count++;
+        }
+        return count;
     }
 }
