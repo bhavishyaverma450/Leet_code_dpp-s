d8e0acaecee0aa51b2c50a6c7b5b77a8a827fb31
BHAVISHYA VERMA <148390371+bhavishyaverma450@users.noreply.github.com>
2026-07-21 15:23:24 +0530

    Time: 95 ms (20.72%), Space: 123.4 MB (5.44%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -4,28 +4,24 @@ class Solution {
         int sum=0;
         for(int t:nums)sum+=t;
         if(sum%2!=0)return false;
-        int target=sum/2;
-        dp=new int[nums.length][target+1];
-        for(int[] t:dp){
-            Arrays.fill(t,-1);
-        }
-        return solve(nums.length-1,nums,target);
-    }
-    public boolean solve(int ind,int[] nums,int target){
-        if(target==0)return false;
-        if(ind==0){
-            return nums[0]==target;
-        }
-        if(dp[ind][target]!=-1){
-            return dp[ind][target]==1;
-        }
-        boolean not_take=solve(ind-1,nums,target);
-        boolean take=false;
-        if(target>=nums[ind]){
-            take=solve(ind-1,nums,target-nums[ind]);
-        }
-        dp[ind][target]=(take||not_take)?1:0;
 
-        return take||not_take;
+        dp=new int[nums.length][sum+1];
+        for(int[] t:dp)Arrays.fill(t,-1);
+
+        return solve(nums.length-1,sum/2,nums);
+    }
+    public boolean solve(int ind,int target,int[] nums){
+        if(target==0)return true;
+        if(ind<0)return false;
+        if(dp[ind][target]!=-1)return dp[ind][target]==1;
+
+        boolean take=false;
+        if(target>=nums[ind])take=solve(ind-1,target-nums[ind],nums);
+        boolean not_take=solve(ind-1,target,nums);
+
+        boolean ans=take||not_take;
+        dp[ind][target]=ans?1:0;
+
+        return ans;
     }
 }
