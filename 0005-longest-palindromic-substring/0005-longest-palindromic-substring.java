76f9ebc11c3deb25d8764329dedebf0759b92631
BHAVISHYA VERMA <148390371+bhavishyaverma450@users.noreply.github.com>
2026-07-24 17:23:31 +0530

    Time: 2119 ms (6.6%), Space: 46.3 MB (40.66%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,27 +1,21 @@
 class Solution {
     public String longestPalindrome(String s) {
         int n=s.length();
-        if(n<=1)return s;
-
-        int maxLen=1;
-        String maxStr=s.substring(0,1);
+        String ans="";
         for(int i=0;i<n;i++){
-            for(int j=i+maxLen;j<n;j++){
-                if(j-i+1>maxLen && isPalin(s.substring(i,j+1))){
-                    maxLen=j-i+1;
-                    maxStr=s.substring(i,j+1);
+            for(int j=i;j<n;j++){
+                if(solve(i,j,s)){
+                    if(j-i+1>ans.length()){
+                        ans=s.substring(i,j+1);
+                    }
                 }
             }
         }
-        return maxStr;
+        return ans;
     }
-    public boolean isPalin(String s){
-        int i=0;
-        int j=s.length()-1;
-        while(i<=j){
-            if(s.charAt(i)!=s.charAt(j))return false;
-            i++;j--;
-        }
-        return true;
+    public boolean solve(int i,int j,String s){
+        if(i>=j)return true;
+        if(s.charAt(i)!=s.charAt(j))return false;
+        return solve(i+1,j-1,s);
     }
 }
