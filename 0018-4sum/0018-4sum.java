7d3f5289a8231bd5c9df87f42577acc5c61de1df
BHAVISHYA VERMA <148390371+bhavishyaverma450@users.noreply.github.com>
2026-09-07 21:01:39 +0530

    Time: 16 ms (93.41%), Space: 45.9 MB (31.72%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -7,24 +7,23 @@ class Solution {
             if(i>0 && nums[i]==nums[i-1])continue;
             for(int j=i+1;j<nums.length-2;j++){
                 if(j>i+1 && nums[j]==nums[j-1])continue;
-                long s=(long)target-nums[i]-nums[j];
+                long s =(long)target-nums[i]-nums[j];
                 int left=j+1;
                 int right=nums.length-1;
                 while(left<right){
-                    long sum=(long) nums[left]+nums[right];
+                    long sum =(long) nums[left]+nums[right];
                     if(s==sum){
                         list.add(Arrays.asList(nums[i],nums[j],nums[left],nums[right]));
-
+                        
                         while(left<right && nums[left]==nums[left+1])left++;
                         while(left<right && nums[right]==nums[right-1])right--;
-
                         left++;
                         right--;
-                    }else if(s<sum)right--;
+                    }else if(sum>s)right--;
                     else left++;
                 }
             }
-        }   
-        return list; 
+        }
+        return list;
     }
 }
